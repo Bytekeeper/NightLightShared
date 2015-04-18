3b46e974abc5ef633c48384e81382244b479b83a
Bytekeeper <therealdante@gmail.com>
2015-04-18 09:40:01 +0200

    Moved messaged to sensors library

--- NightLightShared.h
+++ NightLightShared.h
@@ -1,9 +1,7 @@
 #ifndef _NL_SHARED_
 #define _NL_SHARED_
 
-const uint8_t TYPE_SETTINGS = 0;
-const uint8_t TYPE_TRIGGER = 1;
-const uint8_t TYPE_LIGHT_STATE = 2;
+const uint8_t TYPE_SETTINGS = 200;
 
 struct Settings_t {
   uint8_t type;
@@ -13,15 +11,6 @@ struct Settings_t {
   uint16_t fadeInTenthSecs;
   uint16_t fadeOutTenthSecs;
   uint16_t lightTenthSecs;
-};
-
-struct Trigger_t {
-  uint8_t type;
-};
-
-struct LightState_t {
-  uint8_t type;
-  bool on;
 };
 
 #endif
